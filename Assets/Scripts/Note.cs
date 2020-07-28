438f612d0d0f220d1081359e6304e0235fe7b7d7
clwmrndl92 <clwmrndl92@gmail.com>
2020-07-29 01:19:30 +0900

    Make Game Scene (incomplete)
    No one work... I'll do my best..... Please comback everyone.

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Note : MonoBehaviour
+{
+    public enum Note_Color
+    {
+        Blue,
+        Green,
+        Orange,
+        White
+    }
+    public enum Tile_State
+    {
+        Perfect,
+        Great,
+        Good,
+        Miss
+    }
+    public bool isFall = true;
+    public bool isTouch = false;
+
+    Transform target;
+    SpriteRenderer targetRender;
+
+    Tile_State tileState = Tile_State.Miss;
+    Note_Color color = Note_Color.White;
+    GameObject prevNote = null, nextNote = null;
+
+    private void Awake()
+    {
+        prevNote = SongManager.rescentNote;
+        target = GetComponent<Transform>();
+        targetRender = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        targetRender.color = SongManager.noteColors[(int)this.color];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFall)
+        {
+            noteFall();
+        }
+    }
+
+    void noteFall()
+    {
+        target.Translate(new Vector3(0, (-1) * SongManager.NoteSpeed * Time.deltaTime, 0));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Touch")
+        {
+            isTouch = true;
+        }
+        else if (collision.tag == "Good")
+        {
+            tileState = Tile_State.Good;
+        }
+        else if (collision.tag == "Great")
+        {
+            tileState = Tile_State.Great;
+        }
+        else if (collision.tag == "Perfect")
+        {
+            tileState = Tile_State.Perfect;
+        }
+        else if(collision.tag == "Destroy" && prevNote == null)
+        {
+            destroyTile();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Perfect")
+        {
+            tileState = Tile_State.Great;
+        }
+        else if (collision.tag == "Great")
+        {
+            tileState = Tile_State.Good;
+        }
+        else if (collision.tag == "Good")
+        {
+            tileState = Tile_State.Miss;
+        }
+    }
+
+    public void setColor(Note_Color color)
+    {
+        this.color = color;
+    }
+    public void setNext(GameObject next)
+    {
+        nextNote = next;
+    }
+    public void destroyPrev()
+    {
+        prevNote = null;
+    }
+    public void addScore()
+    {
+        if(tileState == Tile_State.Perfect)
+        {
+            SongManager.combo++;
+            SongManager.score += 10;
+        }
+        else if (tileState == Tile_State.Great)
+        {
+            SongManager.combo++;
+            SongManager.score += 5;
+        }
+        else if (tileState == Tile_State.Good)
+        {
+            SongManager.combo++;
+            SongManager.score += 1;
+        }
+        else if (tileState == Tile_State.Miss)
+        {
+            SongManager.combo = 0;
+        }
+    }
+    public void destroyTile()
+    {
+        addScore();
+        if (nextNote != null)
+        {
+            nextNote.GetComponent<Note>().destroyPrev();
+            SongManager.bottomNote = nextNote;
+        }
+        else
+        {
+            SongManager.rescentNote = null;
+            SongManager.bottomNote = null;
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SongManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public enum State
+{
+    Loading,
+    Play,
+    Pause,
+    End
+}
+
+
+public class SongManager : GameManager
+{
+    public State state = State.Loading;
+    public GameObject notePrefab;
+    public float setNoteSpeed = 100;
+    public float noteInterval = 10;
+    public GameObject[] notePanels;
+    public Color[] setNoteColors;
+    public Text scoreText;
+
+    public static Color[] noteColors;
+    public static int score = 0;
+    public static int combo = 0;
+    public static float NoteSpeed;
+    public static GameObject rescentNote = null;
+    public static GameObject bottomNote = null;
+
+    static int noteNum;
+    float timer, notePosY;
+    float[] notePosx;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        timer = 0.0f;
+        NoteSpeed = setNoteSpeed;
+        noteNum = notePanels.Length;
+        notePosx = new float[noteNum];
+        noteColors = new Color[noteNum+1];
+        for(int i = 0; i < noteNum; ++i)
+        {
+            notePosx[i] = notePanels[i].transform.position.x;
+            noteColors[i] = setNoteColors[i];
+        }
+        noteColors[noteNum] = new Color(255, 255, 255, 255);
+        notePosY = notePanels[0].transform.position.y;
+    }
+    void Start()
+    {
+        loading();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Play)
+        {
+            timer += Time.deltaTime;
+            if (timer > noteInterval/10)
+            {
+                randomNote();
+                timer = 0;
+            }
+            scoreText.text = score.ToString();
+        }
+    }
+
+    int loading()
+    {
+        state = State.Play;
+        return 0;
+    }
+
+    void randomNote()
+    {
+        System.Random random = new System.Random();
+        GameObject tmpNote = Instantiate(notePrefab, new Vector3(notePosx[Random.Range(0, noteNum)], notePosY, 0), Quaternion.identity) as GameObject;
+        if (rescentNote != null)
+        {
+            rescentNote.GetComponent<Note>().setNext(tmpNote);
+        }
+        else
+        {
+            bottomNote = tmpNote;
+        }
+        rescentNote = tmpNote;
+        System.Array values = System.Enum.GetValues(typeof(Note.Note_Color));
+        Note.Note_Color randomColor = (Note.Note_Color)values.GetValue(random.Next(values.Length));
+        tmpNote.GetComponent<Note>().setColor(randomColor);
+    }
+
+    public void Quit(string next_scene)
+    {
+        SceneManager.LoadScene(next_scene);
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+    }
+    public void ReStart()
+    {
+        Time.timeScale = 1.0f;
+    }
+    public void OnClick()
+    {
+        if (bottomNote != null && bottomNote.GetComponent<Note>().isTouch)
+        {
+            bottomNote.GetComponent<Note>().destroyTile();
+        }
+    }
+}
